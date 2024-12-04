3458f9c9a82a6c256265877a635cffc912812ced
Trane20211 <hermes.demicheli@hotmail.com>
2024-12-04 12:42:46 +0100

    Added Filtering

--- webapp/controller/InvoiceList.controller.js
+++ webapp/controller/InvoiceList.controller.js
@@ -4,8 +4,13 @@
 // https://opensource.org/licenses/MIT
 
 sap.ui.define(
-  ["sap/ui/core/mvc/Controller", "sap/ui/model/json/JSONModel"],
-  (Controller, JSONModel) => {
+  [
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator",
+  ],
+  (Controller, JSONModel, Filter, FilterOperator) => {
     "use strict";
 
     return Controller.extend("tutorial.controller.InvoiceList", {
@@ -14,6 +19,21 @@ sap.ui.define(
           currency: "EUR",
         });
         this.getView().setModel(oViewModel, "view");
+      },
+
+      onFilterInvoices(oEvent) {
+        //costruisco l'array dei filtri
+        const aFilter = [];
+        const sQuery = oEvent.getParameter("query");
+        if (sQuery) {
+          aFilter.push(
+            new Filter("ProductName", FilterOperator.Contains, sQuery)
+          );
+        }
+        //Bindo il filtro
+        const oList = this.byId("_IDGenList");
+        const oBinding = oList.getBinding("items");
+        oBinding.filter(aFilter);
       },
     });
   }
