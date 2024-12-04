e5e746ce347e17acf095dac8462403c1c56ebe6f
Trane20211 <hermes.demicheli@hotmail.com>
2024-12-04 15:58:17 +0100

    Added Stars for valuation

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -3,40 +3,56 @@
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
-sap.ui.define(["sap/ui/core/mvc/Controller",
-    "sap/ui/core/routing/History"
-], (Controller, History) => {
-  "use strict";
+sap.ui.define(
+  [
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/core/routing/History",
+    "sap/m/MessageToast",
+  ],
+  (Controller, History, MessageToast) => {
+    "use strict";
 
-  return Controller.extend("tutorial.controller.Detail", {
-    onInit() {
-      const oRouter = this.getOwnerComponent().getRouter();
-      oRouter
-        .getRoute("detail")
-        .attachPatternMatched(this.onObjectMatched, this);
-    },
-
-    onObjectMatched(oEvent) {
-      this.getView().bindElement({
-        path:
-          "/" +
-          window.decodeURIComponent(
-            oEvent.getParameter("arguments").invoicePath
-          ),
-        model: "invoice",
-      });
-    },
-
-    onNavBack() {
-      const oHistory = History.getInstance();
-      const sPreviousHash = oHistory.getPreviousHash();
-
-      if (sPreviousHash !== undefined) {
-        window.history.go(-1);
-      } else {
+    return Controller.extend("tutorial.controller.Detail", {
+      onInit() {
         const oRouter = this.getOwnerComponent().getRouter();
-        oRouter.navTo("overview", {}, true);
-      }
-    },
-  });
-});
+        oRouter
+          .getRoute("detail")
+          .attachPatternMatched(this.onObjectMatched, this);
+      },
+
+      onObjectMatched(oEvent) {
+        this.byId("rating").reset();
+        this.getView().bindElement({
+          path:
+            "/" +
+            window.decodeURIComponent(
+              oEvent.getParameter("arguments").invoicePath
+            ),
+          model: "invoice",
+        });
+      },
+
+      onNavBack() {
+        const oHistory = History.getInstance();
+        const sPreviousHash = oHistory.getPreviousHash();
+
+        if (sPreviousHash !== undefined) {
+          window.history.go(-1);
+        } else {
+          const oRouter = this.getOwnerComponent().getRouter();
+          oRouter.navTo("overview", {}, true);
+        }
+      },
+
+      onRatingChange(oEvent) {
+        const fValue = oEvent.getParameter("value");
+        const oResourceBundle = this.getView()
+          .getModel("i18n")
+          .getResourceBundle();
+        MessageToast.show(
+          oResourceBundle.getText("ratingConfirmation", [fValue])
+        );
+      },
+    });
+  }
+);
